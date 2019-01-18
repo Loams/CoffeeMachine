4ce47d8e525dabc1ef4ef1b4d4e87500e71fcdfe
Stephane Mullings <stephane.mullings@gmail.com>
2019-01-18 23:51:04 +0000

    create DrinkMachineInterface

---
+++ src/Interfaces/DrinkMachineInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CoffeeMachine\Interfaces;
+
+use CoffeeMachine\Exception\HaventEnoughMoney;
+
+Interface DrinkMachineInterface
+{
+    /**
+     * @param DrinkInterface $drink
+     *
+     * @return DrinkInterface
+     */
+    public function commandDrink(DrinkInterface $drink): DrinkInterface;
+
+    /**
+     * @param DrinkInterface $drink
+     *
+     * @return bool
+     * @throws HaventEnoughMoney
+     */
+    public function haveEnoughMoney(DrinkInterface $drink): bool;
+
+    /**
+     * @return void
+     */
+    public function initializeConsumable(): void;
+
+    /**
+     * @return void
+     */
+    public function resetConsumable(): void;
+}
